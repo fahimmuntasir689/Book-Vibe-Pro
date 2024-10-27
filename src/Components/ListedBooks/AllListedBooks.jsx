d918e290db87db7b0ab911877aa53631a5efba34
fahimmuntasir669 <muntasirmozumder689@gmail.com>
2024-10-28 01:20:45 +0600

    listed books are added more

--- src/Components/ListedBooks/AllListedBooks.jsx
+++ src/Components/ListedBooks/AllListedBooks.jsx
@@ -14,15 +14,26 @@ const AllListedBooks = ({ book }) => {
                     <h1 className="text-[#131313] font-semibold text-3xl">{book.bookName}</h1>
                     <h1 className="text-[#131313] font-medium">By : {book.author}</h1>
                     <p className="text-[#131313] font-medium">Tag : <span className="text-[#23BE0A]">#{book.tags[0]}</span></p>
-                        <p><BsCalendarDate className="inline-block"></BsCalendarDate> Date of publishing : {book.publishingTime}</p>
-                        <div className="flex gap-8">
-                            <p><IoPeople className="inline-block"></IoPeople> publisher : {book.publisher}</p>
-                            <p><IoBookSharp className="inline-block"></IoBookSharp> page : {book.numberOfPages}</p>
+                    <p><BsCalendarDate className="inline-block"></BsCalendarDate> Date of publishing : {book.publishingTime}</p>
+                    <div className="flex gap-8">
+                        <p><IoPeople className="inline-block"></IoPeople> publisher : {book.publisher}</p>
+                        <p><IoBookSharp className="inline-block"></IoBookSharp> page : {book.numberOfPages}</p>
+                    </div>
+                    <div className="text-[#FFF] flex gap-4 mt-2">
+                        <div className="bg-[#328EFF] rounded-md p-2">
+                            <p>Category : {book.category}</p>
                         </div>
-                   
+                        <div className="bg-[#FFAC33] rounded-md p-2">
+                            <p>Ratings : {book.rating}</p>
+                        </div>
+                        <div className="bg-[#23BE0A] rounded-md p-2">
+                            <p>View Details</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-           
+
         </>
     );
 };
